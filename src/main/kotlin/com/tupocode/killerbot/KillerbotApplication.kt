31d08c955b57564ac2907081df1477eab8db8434
Anastasiia Zhyrnova <anastasiya.zhyrnova@gmail.com>
2022-01-06 17:20:10 +0200

    added application.properties and TelegramBotConfiguration.kt

--- src/main/kotlin/com/tupocode/killerbot/KillerbotApplication.kt
+++ src/main/kotlin/com/tupocode/killerbot/KillerbotApplication.kt
@@ -1,7 +1,10 @@
 package com.tupocode.killerbot
 
+import com.tupocode.killerbot.configuration.TelegramBotConfiguration
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 class KillerbotApplication
