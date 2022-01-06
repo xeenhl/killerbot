6008fd50d7461f2c0a80803cba47216b36192998
Anastasiia Zhyrnova <anastasiya.zhyrnova@gmail.com>
2022-01-06 17:20:10 +0200

    added application.properties and TelegramBotConfiguration.kt

---
+++ src/main/kotlin/com/tupocode/killerbot/configuration/TelegramBotConfiguration.kt
@@ -0,0 +1,25 @@
+package com.tupocode.killerbot.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+data class TelegramBotConfiguration(
+    @Value("{telegram.bot.token}")
+    var token: String? = null,
+
+    @Value("\${telegram.bot.base.url}")
+    var baseUrl: String? = null,
+
+    @Value("\${telegram.bot.url.token.template}")
+    var tokenUrlTemplate: String? = null,
+
+    @Value("\${telegram.bot.url.get.updates}")
+    var getUpdate: String? = null,
+
+    @Value("\${telegram.bot.url.get.me}")
+    var getMe: String? = null,
+
+    @Value("\${telegram.bot.timeout}")
+    var timeout: Int? = null
+)
