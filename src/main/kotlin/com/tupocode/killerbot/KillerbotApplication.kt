81ee460ebc3f1284b3bc6e001f28f6f0cb942270
Oleh Kozlovskyi <oleh.kozlovskyi@kingfisher.com>
2022-01-06 15:02:55 +0200

    initial commit

---
+++ src/main/kotlin/com/tupocode/killerbot/KillerbotApplication.kt
@@ -0,0 +1,11 @@
+package com.tupocode.killerbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KillerbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<KillerbotApplication>(*args)
+}
---
+++ src/test/kotlin/com/tupocode/killerbot/KillerbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tupocode.killerbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KillerbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
